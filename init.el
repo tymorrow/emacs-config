6ba23f641ddc981bcebdbd6f83ba183374bd2ca8
Ty Morrow <tjm6f4@mst.edu>
2016-01-20 19:58:00 -0600

    Added flake executable path.

--- init.el
+++ init.el
@@ -8,11 +8,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 2)
- '(c-default-style (quote ((c++-mode . "linux") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ '(c-default-style
+   (quote
+    ((c++-mode . "linux")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu"))))
  '(column-number-mode t)
+ '(flycheck-python-flake8-executable "~/.local/bin/flake8")
  '(indent-tabs-mode nil)
  '(inhibit-startup-buffer-menu t)
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/")))))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
